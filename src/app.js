dc16ba8b7a4b34163786214e01c3c240d77177dd
Mark Ribera <markdribera@gmail.com>
2017-09-26 15:35:58 -0500

    update reference to error pug

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   res
     .status(err.status || 500)
-    .render('error', {
+    .render('pages/error', {
       message: err.message,
     });
 });
